6bbd17d75955d542230e7ba2282d76aedc8fedbb
AzumattDev <azumattgaming@gmail.com>
2023-01-06 22:11:51 -0500

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using HarmonyLib;
+
+namespace NoCultivatorDust;
+
+[HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
+static class ZNetSceneAwakePatch
+{
+    static void Postfix(ZNetScene __instance)
+    {
+        // For each PieceTable and for each Piece in each PieceTable remove the place effect
+        var cultivator = __instance.GetPrefab("Cultivator").GetComponent<ItemDrop>();
+        var cultivatorPieceTable = cultivator.m_itemData.m_shared.m_buildPieces;
+        if (cultivatorPieceTable == null) return;
+        foreach (var piece in cultivatorPieceTable.m_pieces)
+        {
+            var prefab = ZNetScene.instance.GetPrefab(Utils.GetPrefabName(piece));
+            if (prefab == null) continue;
+            prefab.TryGetComponent<Piece>(out var pieceComp);
+            if (pieceComp == null) continue;
+            pieceComp.m_placeEffect.m_effectPrefabs = pieceComp.m_placeEffect.m_effectPrefabs
+                .Where(effect => !effect.m_prefab.name.Contains("vfx")).ToArray();
+            NoCultivatorDustPlugin.NoCultivatorDustLogger.LogDebug("Removed build dust from " +
+                                                                   piece.gameObject.name +
+                                                                   " Current list of effect prefabs: " +
+                                                                   string.Join("\n",
+                                                                       pieceComp.m_placeEffect
+                                                                           .m_effectPrefabs
+                                                                           .Select(
+                                                                               effect => effect.m_prefab
+                                                                                   .name)));
+        }
+    }
+}
+
+[HarmonyPatch(typeof(Player), nameof(Player.PlacePiece))]
+static class PiecePlacePiecePatch
+{
+    static void Prefix(Player __instance, ref Piece piece)
+    {
+        // cache the piece.gameObject.name
+        string pieceName = piece.gameObject.name;
+        if (!pieceName.Contains("replant") && !pieceName.Contains("cultivate")) return;
+        NoCultivatorDustPlugin.NoCultivatorDustLogger.LogDebug("Preventing cultivator dust from spawning " + pieceName);
+        piece.m_placeEffect.m_effectPrefabs = piece.m_placeEffect.m_effectPrefabs
+            .Where(effect => !effect.m_prefab.name.Contains("vfx")).ToArray();
+        NoCultivatorDustPlugin.NoCultivatorDustLogger.LogDebug("Removed build dust from " + pieceName +
+                                                               " Current list of effect prefabs: " + string.Join("\n",
+                                                                   piece.m_placeEffect.m_effectPrefabs.Select(
+                                                                       effect => effect.m_prefab.name)));
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoCultivatorDust
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoCultivatorDustPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "NoCultivatorDust";
+        internal const string ModVersion = "1.0.2";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource NoCultivatorDustLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+
+    }
+}
